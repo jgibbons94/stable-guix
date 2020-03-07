788fceecefd8c46c29d3884c0cfb171c943536fc
Jan Nieuwenhuizen <janneke@gnu.org>
2020-03-07 11:12:15 -0500

    gnu: inetutils: Support for the Hurd.
    * gnu/packages/linux.scm (net-tools)[supported-systems]: Remove the Hurd.
    * gnu/packages/patches/inetutils-hurd.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/admin.scm (inetutils): Use it.  Disable some features on the
    Hurd.
    [native-inputs]: Include net-tools only on supported systems

--- gnu/local.mk
+++ gnu/local.mk
@@ -1045,6 +1045,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/id3lib-UTF16-writing-bug.patch			\
   %D%/packages/patches/ilmbase-fix-tests.patch			\
   %D%/packages/patches/ilmbase-openexr-pkg-config.patch		\
+  %D%/packages/patches/inetutils-hurd.patch			\
   %D%/packages/patches/inkscape-poppler-0.76.patch		\
   %D%/packages/patches/intltool-perl-compatibility.patch	\
   %D%/packages/patches/irrlicht-use-system-libs.patch		\
