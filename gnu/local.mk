cc63c422440b8f191d58dba9b223c810d8ec0243
Jan Nieuwenhuizen <janneke@gnu.org>
2020-03-07 14:55:35 +0100

    gnu: less: Build fix for the Hurd.
    * gnu/packages/patches/less-hurd-path-max.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/less.scm (less): Use it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1100,6 +1100,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/ldc-bootstrap-disable-tests.patch	\
   %D%/packages/patches/ldc-disable-phobos-tests.patch		\
   %D%/packages/patches/leela-zero-gtest.patch			\
+  %D%/packages/patches/less-hurd-path-max.patch			\
   %D%/packages/patches/liba52-enable-pic.patch			\
   %D%/packages/patches/liba52-link-with-libm.patch		\
   %D%/packages/patches/liba52-set-soname.patch			\
